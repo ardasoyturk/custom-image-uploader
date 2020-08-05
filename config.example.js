bad27a8545d7b8fb8448f44813df9df8c3d64dc2
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2020-08-05 13:58:55 +0300

    added server and config example

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "token": "superdupertokengoeshere",
+    "domain": "https://example.com/", // dont forget the / at the end
+    "fieldname": "file"
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const config = require('./config.js');
+app.use(express.static('i'));
+app.enable('trust proxy');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/i')
+  },
+  filename: function (req, file, cb) {
+    let str = makeid(5) + '.' + file.originalname.split('.')[1]
+    cb(null, str)
+  }
+})
+var upload = multer({ storage })
+
+app.post('/upload', upload.single(config.fieldname), (req, res) => {
+  let token = req.headers['key'];
+  if (token !== config.token) return res.sendStatus(403).send('Forbidden').end(); 
+  let str = config.domain + req.file.filename;
+  res.json({ url: str }).end()
+})
+
+function makeid(length) {
+   var result = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+
+app.listen(3000, () => console.log('Listening...'))
